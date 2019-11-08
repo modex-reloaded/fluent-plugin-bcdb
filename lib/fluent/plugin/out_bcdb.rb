0762d4e77c2eb8c2be519360b76615ed8a4b4a84
dorin.stan@modex.tech <agabriel777@gmail.com>
2019-11-08 17:45:49 +0200

    update license

--- lib/fluent/plugin/out_bcdb.rb
+++ lib/fluent/plugin/out_bcdb.rb
@@ -1,5 +1,5 @@
 #
-# Copyright ©2019. Modex. | MODEX (Gibraltar) LIMITED
+# Copyright ©2019. MODEX (Gibraltar) LIMITED
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
